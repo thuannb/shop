3a8ba1a778d39180a9801863d3dc116d3ee3ed1b
THUANNB-STS\THUANNB <thuannbsts@gmail.com>
2017-08-21 22:08:42 +0700

    Use toastr để notification

--- Shop.Web/app/components/productCategories/productCategoryListController.js
+++ Shop.Web/app/components/productCategories/productCategoryListController.js
@@ -1,9 +1,9 @@
 ﻿(function (app) {
 	app.controller('productCategoryListController', productCategoryListController);
 
-	productCategoryListController.$inject = ['$scope', 'apiService'];
+	productCategoryListController.$inject = ['$scope', 'apiService','notificationService'];
 
-	function productCategoryListController($scope, apiService) {
+	function productCategoryListController($scope, apiService, notificationService) {
 		$scope.productCategories = [];
 		$scope.page = 0;
 		$scope.pagesCount = 0;
@@ -27,6 +27,12 @@
 			}
 
 			apiService.get('/api/productcategory/getall', config, function (result) {
+				if (result.data.TotalCount === 0) {
+					notificationService.displayWarning('Không tìm thấy dữ liệu bạn tìm!', 'Cảnh báo');
+				}
+				else {
+					notificationService.displaySuccess('Dữ liệu đã được tìm thấy!', 'Thông báo');
+				}
 				$scope.productCategories = result.data.Items;
 				$scope.page = result.data.Page;
 				$scope.pagesCount = result.data.PagesCount;
---
+++ Shop.Web/app/shared/services/notificatonService.js
@@ -0,0 +1,56 @@
+﻿/// <reference path="~/Assets/admin/libs/angular/angular.js" />
+(function (app) {
+
+	app.factory('notificationService', notificationService);
+
+	function notificationService() {
+		toastr.options = {
+			"closeButton": true,
+			"debug": false,
+			"newestOnTop": false,
+			"progressBar": false,
+			"positionClass": "toast-top-right",
+			"preventDuplicates": false,
+			"onclick": null,
+			"showDuration": "300",
+			"hideDuration": "1000",
+			"timeOut": "5000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+			"showMethod": "fadeIn",
+			"hideMethod": "fadeOut"
+		}
+
+		return {
+			displaySuccess: displaySuccess,
+			displayError: displayError,
+			displayWarning: displayWarning,
+			displayInfo: displayInfo
+		}
+
+		function displaySuccess(message, title) {
+			toastr.success(message, title);
+		}
+
+		function displayError(error, title) {
+			if (Array.isArray) {
+				error.each(function (err) {
+					toastr.error(err, title);
+				})
+			}
+			else {
+				toastr.error(error, title);
+			}
+		}
+
+		function displayWarning(message, title) {
+			toastr.warning(message, title);
+		}
+
+		function displayInfo(message, title) {
+			toastr.info(message, title);
+		}
+	}
+
+})(angular.module('shopapp.common'))
